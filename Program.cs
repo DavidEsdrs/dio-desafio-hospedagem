76759b483026337d811986381dd6dc247e10f656
DavidEsdrs <carpina1983@gmail.com>
2024-05-14 22:01:56 -0300

    feat: set basic operations for creating and reading entities

--- Program.cs
+++ Program.cs
@@ -1,25 +1,69 @@
 ﻿using System.Text;
 using DesafioProjetoHospedagem.Models;
+using Newtonsoft.Json;
+using Utils;
+
+Setup.Execute();
+
+List<Suite> suites = [];
+List<Pessoa> pessoas = [];
+List<Reserva> reservas = [];
+
+JsonConvert.DeserializeObject<List<Suite>>(File.ReadAllText(Configs.SuitesArquivo));
+JsonConvert.DeserializeObject<List<Suite>>(File.ReadAllText(Configs.SuitesArquivo));
+JsonConvert.DeserializeObject<List<Suite>>(File.ReadAllText(Configs.SuitesArquivo));
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+int opcao;
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+do {
+  Console.Write("Escolha sua opção > ");
+  opcao = Convert.ToInt32(Console.ReadLine());
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+  string content;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
-
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
-
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+  switch (opcao) {
+    case 1:
+      Console.WriteLine("Criando suíte");
+      Suite s = Controller.AdicionarSuite();
+      suites.Add(s);
+      content = JsonConvert.SerializeObject(suites, Formatting.Indented);
+      File.WriteAllText(Configs.SuitesArquivo, content);
+      break;
+    case 2:
+      Console.WriteLine("Adicionando pessoa");
+      Pessoa p = Controller.AdicionarPessoa();
+      pessoas.Add(p);
+      content = JsonConvert.SerializeObject(pessoas, Formatting.Indented);
+      File.WriteAllText(Configs.PessoasArquivo, content);
+      break;
+    case 3:
+      Console.WriteLine("Criar reserva");
+      Reserva r = Controller.CriarReserva();
+      reservas.Add(r);
+      content = JsonConvert.SerializeObject(pessoas, Formatting.Indented);
+      File.WriteAllText(Configs.ReservasArquivo, content);
+      break;
+    case 4:
+      Console.WriteLine("Ver suítes");
+      foreach(var suite in suites) {
+        Console.WriteLine(suite);
+      }
+      break;
+    case 5:
+      Console.WriteLine("Ver pessoas");
+      foreach(var pessoa in pessoas) {
+        Console.WriteLine(pessoa);
+      }
+      break;
+    case 6:
+      Console.WriteLine("Ver reservas");
+      foreach(var reserva in reservas) {
+        Console.WriteLine(reserva);
+      }
+      break;
+    default:
+      break;
+  }
+} while(opcao != 0);
